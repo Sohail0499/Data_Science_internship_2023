124c716a63d9712c2267dde5d72a8e34cbc7b24d
Sohail Shaikh <50219669+Sohail0499@users.noreply.github.com>
2023-02-27 16:35:40 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,14 +4,14 @@ import streamlit as st
 st.title("EMI Calculator")
 
 # User inputs
-principal = st.number_input("Enter the loan amount (in rupees)")
-interest_rate = st.slider("Enter the interest rate (in %)", min_value=1.0, max_value=15.0, step=0.1)
-tenure = st.slider("Enter the tenure (in years)", min_value=1.0, max_value=30.0, step=1.0)
+principal = st.number_input("Enter the loan amount (in rupees)", step=1)
+interest_rate = st.slider("Enter the interest rate (in %)", min_value=6, max_value=15, step=1)
+tenure = st.slider("Enter the tenure (in years)", min_value=1, max_value=30, step=1)
 
 # Calculate EMI
 interest_rate = interest_rate / (12 * 100)
 tenure_months = tenure * 12
-emi = (principal * interest_rate * (1 + interest_rate)**tenure_months) / ((1 + interest_rate)**tenure_months - 1)
+emi = int((principal * interest_rate * (1 + interest_rate)**tenure_months) / ((1 + interest_rate)**tenure_months - 1))
 
 # Display EMI
-st.write(f"The monthly EMI is {emi:.2f} rupees")
+st.write(f"The monthly EMI is {emi:} rupees")
